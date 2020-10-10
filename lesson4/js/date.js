600a1326b2bacfd5efeae55038466e62e8d831d7
prattsbyui <pratts@byui.edu>
2020-10-10 06:10:26 -0500

    week4

---
+++ lesson4/js/date.js
@@ -0,0 +1,10 @@
+document.getElementById("updated").innerHTML = document.lastModified
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
