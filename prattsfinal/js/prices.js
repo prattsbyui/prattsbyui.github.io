5c5ffec10d2aa768290c55bdd91733580f09b10a
prattsbyui <pratts@byui.edu>
2020-12-15 16:27:36 -0600

    final

---
+++ prattsfinal/js/prices.js
@@ -0,0 +1,32 @@
+const scoots = "/prattsfinal/data/scoots.json";
+
+fetch(scoots)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const prices = jsonObject["prices"]
+    for(let i = 0; i < prophets.length; i++) {
+
+        let card = document.createElement("section");
+        let h2 = document.createElement("h2");
+        let birth = document.createElement("p");
+        let place = document.createElement("p");
+        let image = document.createElement("img");
+        let imageAlt = prophets[i].name + " " + prophets[i].lastname + " - " + prophets[i].order;
+
+        h2.textContent = prophets[i].name + " " + prophets[i].lastname;
+        birth.textContent = "Date of birth: " + prophets[i].birthdate;
+        place.textContent = "Place of birth: " + prophets[i].birthplace;
+        image.setAttribute("src", prophets[i].imageurl);
+        image.setAttribute("alt", imageAlt);
+        
+        card.appendChild(h2);
+        card.appendChild(birth);
+        card.appendChild(place);
+        card.appendChild(image);
+
+        document.querySelector("div.cards").appendChild(card);
+
+    }
+  });
