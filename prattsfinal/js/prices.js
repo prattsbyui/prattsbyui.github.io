279d26563985bb4209c1cfd502749c02939a0b1c
prattsbyui <pratts@byui.edu>
2020-12-15 17:43:57 -0600

    final

--- prattsfinal/js/prices.js
+++ prattsfinal/js/prices.js
@@ -1,30 +1,54 @@
-const scoots = "/prattsfinal/data/scoots.json";
+const requestURL = "https://prattsbyui.github.io/prattsfinal/data/scoots.json";
 
-fetch(scoots)
+fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
     const prices = jsonObject["prices"]
-    for(let i = 0; i < prophets.length; i++) {
+    for(let i = 0; i < prices.length; i++) {
 
         let card = document.createElement("section");
-        let h2 = document.createElement("h2");
-        let birth = document.createElement("p");
-        let place = document.createElement("p");
-        let image = document.createElement("img");
-        let imageAlt = prophets[i].name + " " + prophets[i].lastname + " - " + prophets[i].order;
 
-        h2.textContent = prophets[i].name + " " + prophets[i].lastname;
-        birth.textContent = "Date of birth: " + prophets[i].birthdate;
-        place.textContent = "Place of birth: " + prophets[i].birthplace;
-        image.setAttribute("src", prophets[i].imageurl);
-        image.setAttribute("alt", imageAlt);
+        let picture = document.createElement("div")
+        let h2 = document.createElement("h2");
+        let image = document.createElement("img");
+        let imageAlt = prices[i].name;
+        let persons = document.createElement("p")
+
+        let reservation = document.createElement("div")
+ let reserve = document.createElement("h3");
+let rhalf = document.createElement("p")
+let rfull = document.createElement("p")
+        let walkin = document.createElement("div")
         
-        card.appendChild(h2);
-        card.appendChild(birth);
-        card.appendChild(place);
-        card.appendChild(image);
+       
+        
+        let founded = document.createElement("p");
+        let population = document.createElement("p");
+        let rain = document.createElement("p");
+
+
+        h2.textContent = prices[i].RentalType;
+        image.setAttribute("src", "/prattsfinal/images/" + prices[i].photo);
+        image.setAttribute("alt", imageAlt);
+        persons.textContent = "Max Riders: " + prices[i].MaxPersons;
+
+        reserve.textContent = "Reservation";
+rhalf.textContent = "Half-Day(3hrs): $" + prices[i].Reservation.HalfDay;
+rfull.textContent = "Full Day: $" + prices[i].Reservation.FullDay;
+           
+        picture.appendChild(h2);
+        picture.appendChild(image);
+        picture.appendChild(persons);
+
+reservation.appendChild(reserve);
+reservation.appendChild(rhalf);
+reservation.appendChild(rfalf);
+
+        card.appendChild(picture);
+        card.appendChild(reservation)
+     
 
         document.querySelector("div.cards").appendChild(card);
 
